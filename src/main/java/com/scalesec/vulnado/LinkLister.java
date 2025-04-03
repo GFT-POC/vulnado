b9c704d33c2ff2c311ed5e1c80b7fbddcbd97885
GFT-POC <sebastien.martineau.gft@gmail.com>
2025-04-03 08:13:14 -0400

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/LinkLister.java

--- src/main/java/com/scalesec/vulnado/LinkLister.java
+++ src/main/java/com/scalesec/vulnado/LinkLister.java
@@ -11,8 +11,11 @@ import java.net.*;
 
 
 public class LinkLister {
+  private LinkLister() {
   public static List<String> getLinks(String url) throws IOException {
-    List<String> result = new ArrayList<String>();
+    // Private constructor to hide the implicit public one
+    List<String> result = new ArrayList<>();
+  }
     Document doc = Jsoup.connect(url).get();
     Elements links = doc.select("a");
     for (Element link : links) {
@@ -25,7 +28,8 @@ public class LinkLister {
     try {
       URL aUrl= new URL(url);
       String host = aUrl.getHost();
-      System.out.println(host);
+      Logger logger = Logger.getLogger(LinkLister.class.getName());
+      logger.info(host);
       if (host.startsWith("172.") || host.startsWith("192.168") || host.startsWith("10.")){
         throw new BadRequest("Use of Private IP");
       } else {
